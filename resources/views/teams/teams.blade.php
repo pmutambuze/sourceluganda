45e2cde9dc9530e4631b143777193481b57990f0
Mutambuze Paul <paulmutambuze@gmail.com>
2020-07-10 22:17:43 +0300

    Team Edit

--- resources/views/teams/teams.blade.php
+++ resources/views/teams/teams.blade.php
@@ -1,9 +1,62 @@
+
+ <!-- ======= Team Section ======= -->
+ <section id="team" class="team section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2 data-aos="fade-up">Editorial Team</h2>
+          <p data-aos="fade-up">
+            Alone we can do so little yet together we can do so much.
+          </p>
+        </div>
+
+        <div class="row">
+                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Miss. Najjuma Veronica </h4>
+                <span>Chief Editor</span>
+              </div>
+            </div>
+          </div>
+                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Mr. Luyinda Edrine </h4>
+                <span>Illustrator</span>
+              </div>
+            </div>
+          </div>
+                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Mr. Katava Isaac </h4>
+                <span>Langauge Editor</span>
+              </div>
+            </div>
+          </div>
+                  </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
 <!-- ======= Team Section ======= -->
+
+<!-- 
     <section id="team" class="team section-bg">
       <div class="container">
 
         <div class="section-title">
-          <h2 data-aos="fade-up">Journal Editorial Members</h2>
+          <h2 data-aos="fade-up">Editorial Members</h2>
           <p data-aos="fade-up">
             Alone we can do so little; together we can do so much
           </p>
@@ -26,4 +79,5 @@
         </div>
 
       </div>
-    </section><!-- End Team Section -->
+    </section><!-- End Team Section --> -->
+ -->
