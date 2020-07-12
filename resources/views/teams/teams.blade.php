942821e004de007c6ac18690392f7d91adc29673
Mutambuze Paul <paulmutambuze@gmail.com>
2020-07-12 11:52:36 +0300

    page

--- resources/views/teams/teams.blade.php
+++ resources/views/teams/teams.blade.php
@@ -49,10 +49,10 @@
   <section id="team" class="team section-bg">
       <div class="container">
         <div class="section-title">
-          <h2 data-aos="fade-up">Editorial Team</h2>
-          <p data-aos="fade-up">
+          <h2 data-aos="fade-up">Our Team</h2>
+          <!-- <p data-aos="fade-up">
             Alone we can do so little yet together we can do so much.
-          </p>
+          </p> -->
         </div>
         <div class="row">
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
@@ -61,8 +61,11 @@
                 <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
               </div>
               <div class="member-info">
-                <h4>Najjuma Veronica </h4>
-                <span>Chief Editor</span>
+                <h4>Owek. Dr. Prosperous Nankindu </h4>
+                <span>Team Leader</span>s
+                 <p data-aos="fade-up">
+            Dr. Prosperous Nankindu is Minister for Social Services (in charge of Health, Education and Gender) in the Kingdom of Buganda She is an expert in language education and linguistics, a qualified teacher, Assessment Expert, Teacher trainer and Researcher with considerable experience in mixed research methods. Prosperous is also the Founder Member and Director of Sir Edward Foundation, a Community Based Organization for Women, Youth and Children. She is keen on embracing new approaches to learning, has zeal for interdisciplinary approaches to knowing, an enthusiastic pursuer of opportunities for team membership, keen on lessons to learn out of all situations, an individual with great ability for multitasking without losing focus of desired goals and targeted outcomes. 
+          </p> 
               </div>
             </div>
           </div>
