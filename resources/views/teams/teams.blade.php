7aee4c2a2b9fe82f2091714aa55f29c3debc9a16
Mutambuze Paul <paulmutambuze@gmail.com>
2020-07-12 11:46:26 +0300

    members

--- resources/views/teams/teams.blade.php
+++ resources/views/teams/teams.blade.php
@@ -2,16 +2,14 @@
  <!-- ======= Team Section ======= -->
  <section id="team" class="team section-bg">
       <div class="container">
-
         <div class="section-title">
           <h2 data-aos="fade-up">Editorial Team</h2>
           <p data-aos="fade-up">
             Alone we can do so little yet together we can do so much.
           </p>
         </div>
-
         <div class="row">
-                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="member">
               <div class="member-img">
                 <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
@@ -22,7 +20,7 @@
               </div>
             </div>
           </div>
-                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="member">
               <div class="member-img">
                 <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
@@ -33,7 +31,7 @@
               </div>
             </div>
           </div>
-                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="member">
               <div class="member-img">
                 <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
@@ -44,41 +42,53 @@
               </div>
             </div>
           </div>
-                  </div>
-
+        </div>
       </div>
-    </section>
-    <!-- End Team Section -->
+  </section>
 
-<!-- ======= Team Section ======= -->
-
-<!-- 
-    <section id="team" class="team section-bg">
+  <section id="team" class="team section-bg">
       <div class="container">
-
         <div class="section-title">
-          <h2 data-aos="fade-up">Editorial Members</h2>
+          <h2 data-aos="fade-up">Editorial Team</h2>
           <p data-aos="fade-up">
-            Alone we can do so little; together we can do so much
+            Alone we can do so little yet together we can do so much.
           </p>
         </div>
-
         <div class="row">
-          @foreach($members as $member)
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
             <div class="member">
               <div class="member-img">
                 <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
               </div>
               <div class="member-info">
-                <h4>{{$member->name}}</h4>
-                <span>{{$member->title}}</span>
+                <h4>Najjuma Veronica </h4>
+                <span>Chief Editor</span>
               </div>
             </div>
           </div>
-          @endforeach
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Luyinda Edrine </h4>
+                <span>Illustrator</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+            <div class="member">
+              <div class="member-img">
+                <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
+              </div>
+              <div class="member-info">
+                <h4>Katava Isaac </h4>
+                <span>Langauge Editor</span>
+              </div>
+            </div>
+          </div>
         </div>
-
       </div>
-    </section><!-- End Team Section --> -->
- -->
+  </section>
+    <!-- End Team Section -->
